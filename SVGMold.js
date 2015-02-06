9cd5661be74d399fcdd6d8d3d29f5566f1b2813d
Kostas <kostas@creativecollective.gr>
2015-02-06 19:03:36 +0200

    Update SVGMold.js

--- SVGMold.js
+++ SVGMold.js
@@ -1,6 +1,6 @@
 (function(window,doc){
 
-  var SVGMold = function( type, options ){
+  var SVGMold = function SVGMold( type, options ){
     var molding = document.createElementNS('http://www.w3.org/2000/svg', type );
     for (var key in options) options.hasOwnProperty(key) && molding.setAttribute( key, options[key]);
     return molding;
