ff275d116c74e627e571e7627f36141d4a574a66
Kostas <kostas@creativecollective.gr>
2015-02-06 18:56:28 +0200

    Create SVGMold.js

---
+++ SVGMold.js
@@ -0,0 +1,11 @@
+(function(window,doc){
+
+  var SVGMold = function( type, options ){
+    var molding = document.createElementNS('http://www.w3.org/2000/svg', type );
+    for (var key in options) options.hasOwnProperty(key) && molding.setAttribute( key, options[key]);
+    return molding;
+  }
+
+  return window.SVGMold = SVGMold;
+
+})(window,document);
